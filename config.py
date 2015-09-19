bcfe2b9602e296062562144607241fddc0f59c81
Mike Yavorsky <me@mikeyavorsky.com>
2015-09-19 02:08:00 -0700

    Added SQLAlchemy to config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '7DBTk9pUKtkDi7HL,aA]s(9aGULmr(UWme2q8z$uPy+UgaPZUM'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
