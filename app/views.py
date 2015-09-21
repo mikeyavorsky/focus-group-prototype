7b45d7328ffbc0218858ec288f3ea951ff09dea4
Mike Yavorsky <me@mikeyavorsky.com>
2015-09-21 15:24:53 -0700

    Added post view and changed index template

--- app/views.py
+++ app/views.py
@@ -1,36 +1,32 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}
     posts = [
         {
-            'author': {'nickname': 'John'},
+            'author': 'John',
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'nickname': 'Susan'},
+            'author': 'Susan',
             'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html',
                            title='Home',
-                           user=user,
                            posts=posts)
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+@app.route('/post/')
+@app.route('/post/<id>')
+def post():
+    post = {
+            'author': 'Mike',
+            'title': 'Test Post',
+            'body': 'This is the only post in this list of dicts'
+    }
+    return render_template('post.html',
+                           title=post['title'],
+                           post=post)
