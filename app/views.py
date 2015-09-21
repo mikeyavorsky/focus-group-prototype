169ead7b2c7de3d39220153b1f5e5e70fa5092e2
Mike Yavorsky <me@mikeyavorsky.com>
2015-09-21 15:36:13 -0700

    Added dummy content for posts, including questions, comments, and timestamps

--- app/views.py
+++ app/views.py
@@ -7,26 +7,89 @@ from app import app
 def index():
     posts = [
         {
+            'id': '1',
+            'timestamp': '2012-12-31 15:54:42.915204',
             'author': 'John',
-            'body': 'Beautiful day in Portland!'
+            'body': 'Beautiful day in Portland!',
+            'questions': [
+                {
+                  'type': 'integer',
+                  'body': 'how much do you love Portland (1-5)?'
+                },
+                {
+                  'type': 'string',
+                  'body': 'What do you love most about Portland?'
+                }
+            ]
         },
         {
+            'id': '2',
+            'timestamp': '2012-12-30 15:54:42.915204',
             'author': 'Susan',
-            'body': 'The Avengers movie was so cool!'
+            'body': 'The Avengers movie was so cool!',
+            'questions': [
+                {
+                  'type': 'integer',
+                  'body': 'how much do you love Portland (1-5)?'
+                },
+                {
+                  'type': 'string',
+                  'body': 'What do you love most about Portland?'
+                }
+            ] 
+        }
+    ]
+    comments = [
+        {
+            'id': '1',
+            'post_id': '2',
+            'timestamp': '2012-12-31 15:54:42.915204',
+            'author': 'Roger',
+            'body': 'I didn't really like the Avengers movie, nor seeing it in Portland.'
+        },
+        {
+            'id': '2',
+            'post_id': '1',
+            'timestamp': '2012-12-30 15:54:42.915204',
+            'author': 'Christine',
+            'body': 'I loved the food in Portland! Shwarma! Just like the Avengers'
         }
     ]
     return render_template('index.html',
                            title='Home',
-                           posts=posts)
+                           posts=posts
+                           comments=comments)
 
 @app.route('/post/')
 @app.route('/post/<id>')
 def post():
-    post = {
-            'author': 'Mike',
-            'title': 'Test Post',
-            'body': 'This is the only post in this list of dicts'
-    }
+    post = [
+        {
+            'id': '2',
+            'timestamp': '2012-12-30 15:54:42.915204',
+            'author': 'Susan',
+            'body': 'The Avengers movie was so cool!',
+            'questions': [
+                {
+                  'type': 'integer',
+                  'body': 'how much do you love Portland (1-5)?'
+                },
+                {
+                  'type': 'string',
+                  'body': 'What do you love most about Portland?'
+                }
+            ]
+        }
+    ]
+    comments = [
+        {
+            'id': '1',
+            'post_id': '2',
+            'author': 'Roger',
+            'body': 'I didn't really like the Avengers movie, nor seeing it in Portland.'
+        }
+    ]
     return render_template('post.html',
                            title=post['title'],
-                           post=post)
+                           post=post,
+                           comments=comments)
