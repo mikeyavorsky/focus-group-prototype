31cb0addc391acd6f80e82ac629ca5000a90876a
Mike Yavorsky <me@mikeyavorsky.com>
2015-09-22 05:11:57 -0700

    Decoupled questions and posts. Working post submission and comment submission.

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,20 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, RadioField, IntegerField
+from wtforms import StringField, BooleanField, RadioField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired
 from datetime import datetime
 
 class CommentForm(Form):
     author = StringField('author', validators=[DataRequired()])
-    body = StringField('body', validators=[DataRequired()])
+    body = TextAreaField('body', validators=[DataRequired()])
 
 class PostForm(Form):
     title = StringField('title', validators=[DataRequired()])
     author = StringField('author', validators=[DataRequired()])
-    body = StringField('body', validators=[DataRequired()])
-    num_questions = IntegerField('num_questions')
+    body = TextAreaField('body', validators=[DataRequired()])
 
-class AddQuestionForm(Form):
-    type = RadioField(u'Type', choices=[
+class QuestionForm(Form):
+    qtype = RadioField(u'Type', choices=[
         ('1', u'You want the respondent to write out an answer.'),
         ('2', u'You want the respondent to rank something from 1 to 5.')],
         default='1', validators=[DataRequired()])
-    body = StringField('body', validators=[DataRequired()])
+    body = TextAreaField('body', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,37 +1,39 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
 from .models import Post, Comment, Question, Answer, Response
-from .forms import PostForm, CommentForm, AddQuestionForm
+from .forms import PostForm, CommentForm, QuestionForm
 from datetime import datetime
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    form = CommentForm()
-    if form.validate_on_submit():
-        comment = comment(post_id=Post.query.order_by(Post.timestamp.desc())[0].id,
-                          author=form.author.data,
-                          body=form.body.data)
-        db.session.add(comment)
-        db.session.commit()
-        flash('Your comment has been posted!')
-        return redirect(url_for('index'))
+    #form = CommentForm()
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html',
                            title='Home',
                            posts=posts)
 
-@app.route('/post/')
-@app.route('/post/<post_id>')
+@app.route('/post/', methods=['GET','POST'])
+@app.route('/post/<post_id>', methods=['GET','POST'])
 def post(post_id=0):
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(post_id=Post.query.order_by(Post.timestamp.desc())[0].id,
+                          author=form.author.data,
+                          body=form.body.data)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been posted!')
+        return redirect(url_for('post', post_id=post_id))
     if post_id == 0:
         post = Post.query.order_by(Post.timestamp.desc())
     else:
         post = Post.query.filter_by(id=post_id)
     return render_template('post.html',
                            title=post[0].title,
-                           post=post[0])
+                           post=post[0],
+                           form=form)
 
 
 @app.route('/admin')
@@ -64,34 +66,27 @@ def new_post():
         post = Post(title=form.title.data,
                     author=form.author.data,
                     body=form.body.data)
-        num_questions = form.num_questions.data
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
-        if num_questions > 0:
-            return redirect(url_for('add-question'),
-                            title='Add Question',
-                            form=form)
-        else:
-            return redirect(url_for('index'))
+        return redirect(url_for('admin'))
     return render_template('new-post.html',
                            title='New Post',
                            form=form)
 
-@app.route('/add-question', methods=['GET','POST'])
-def add_questions():
-    form = AddQuestionForm()
+@app.route('/new-question', methods=['GET','POST'])
+def new_question():
+    form = QuestionForm()
     if form.validate_on_submit():
-        question = Question(title=form.title.data,
-                            author=form.author.data,
-                            body=form.body.data,
-                            post_id=post[0].id)
+        posts = Post.query.order_by(Post.timestamp.desc())
+        post_id = posts[0].id
+        question = Question(post_id=post_id,
+                            type=form.qtype.data,
+                            body=form.content.data)
         db.session.add(question)
         db.session.commit()
-        flash('Added a question!')
-        return redirect(url_for('add-question'),
-                        title='Add Question',
-                        form=form)
-    return render_template('add-question.html',
-                           title='Add Question',
+        flash('You\'ve added a question!')
+        return redirect(url_for('admin'))
+    return render_template('new-question.html',
+                           title='New Question',
                            form=form)
